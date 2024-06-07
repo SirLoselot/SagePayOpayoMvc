53aa1e57a7f3612727ab690a3ab9d50cc69d5eef
SirLoselot <gulfaran@gmail.com>
2024-06-07 12:18:47 +0100

    Update Configuration.cs
    Changed the URLs according to the notification sent out by OPAYO
    
    The simulator and refund URLs have not been advertised so they have been changed on the basis of the format for the Live and Test URLs that are given on this page:
    
    https://developer.elavon.com/products/opayo-direct/v1/testing#test-urls

--- src/SagePayMvc/Configuration.cs
+++ src/SagePayMvc/Configuration.cs
@@ -38,13 +38,13 @@ namespace SagePayMvc {
 		public const decimal DefaultVatMultiplier = 1.2m;
 		public const string DefaultResponseAction = "Index";
 
-		public const string LiveUrl = "https://live.sagepay.com/gateway/service/vspserver-register.vsp";
-		public const string TestUrl = "https://test.sagepay.com/gateway/service/vspserver-register.vsp";
-		public const string SimulatorUrl = "https://test.sagepay.com/simulator/VSPServerGateway.asp?Service=VendorRegisterTx";
+		public const string LiveUrl = "https://live.opayo.eu.elavon.com/gateway/service/vspdirect-register.vsp";
+		public const string TestUrl = "https://sandbox.opayo.eu.elavon.com/gateway/service/vspdirect-register.vsp";
+		public const string SimulatorUrl = "https://sandbox.opayo.eu.elavon.com/simulator/VSPServerGateway.asp?Service=VendorRegisterTx";
 
-		public const string LiveRefundUrl = "https://live.sagepay.com/gateway/service/refund.vsp";
-		public const string TestRefundUrl = "https://test.sagepay.com/gateway/service/refund.vsp";
-		public const string SimulatorRefundUrl = "https://test.sagepay.com/simulator/vspserverGateway.asp?Service=VendorRefundTx";
+		public const string LiveRefundUrl = "https://live.opayo.eu.elavon.com/gateway/service/refund.vsp";
+		public const string TestRefundUrl = "https://sandbox.opayo.eu.elavon.com/gateway/service/refund.vsp";
+		public const string SimulatorRefundUrl = "https://sandbox.opayo.eu.elavon.com/simulator/vspserverGateway.asp?Service=VendorRefundTx";
 
 		string notificationController = DefaultControllerName;
 		string notificationAction = DefaultResponseAction;
